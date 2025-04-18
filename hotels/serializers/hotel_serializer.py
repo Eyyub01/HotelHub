eebc26e52f7ac88979efbb0b37f2b29dbe7a7fe5
Eyyub01 <abbaszadeeyyub@gmail.com>
2025-04-18 21:52:26 +0400

    merge

---
+++ hotels/serializers/hotel_serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from hotels.models.hotel_models import Hotel
+from hotels.serializers.hotel_photo_serializer import HotelPhotoSerializer
+
+
+class HotelSerializer(serializers.ModelSerializer):
+    country = serializers.CharField(source='city.country', read_only=True)  
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)  
+    photo = HotelPhotoSerializer(many=True, read_only=True, source='photos')
+
+    class Meta:
+        model = Hotel
+        fields = [
+            'id', 'name', 'address', 'phone', 'email', 'description', 
+            'star_rating', 'check_in_time', 'check_out_time', 'city', 
+            'country', 'owner', 'created_at', 'updated_at'
+        ]
