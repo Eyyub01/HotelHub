6c0e48e08c57f90eaf36f849f84c153c1ad0d1ee
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2025-04-26 10:10:46 +0400

    ADD django channels with websocker feature

--- hotelhub/urls.py
+++ hotelhub/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/v1/', include('accounts.urls')),
     path('api/v1/', include('rooms.urls')),
     path('api/v1/', include('bookings.urls')),
+    path("chat/", include("chat.urls")),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
